58c00e0bb2efbdb4774d73b9d9fbdf3fb81f4041
vitor <vitorhugkrodrigues@gmail.com>
2025-07-19 15:46:10 -0300

    new portfolio

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+// const svg = document.querySelector('svg.squiggle')
+// const path = svg.querySelector('path')
+
+// const scroll = () => {
+//   const distance = window.scrollY
+//   const totalDistance = svg.clientHeight - window.innerHeight
+
+//   const percentage = distance / totalDistance
+
+//   const pathLength = path.getTotalLength()
+
+//   path.style.strokeDasharray = `${pathLength}`
+//   path.style.strokeDashoffset = `${pathLength * (1 - percentage)}`
+// }
+
+// scroll()
+// window.addEventListener('scroll', scroll)
+
+const svg = document.querySelector('svg.squiggle')
+const path = svg.querySelector('path')
+
+const scroll = () => {
+  const distance = window.scrollY
+  const pathLength = path.getTotalLength()
+
+  const speedFactor = 1.8 // ajuste aqui: quanto maior, mais aparece a cada scroll
+
+  let offset = pathLength - distance * speedFactor
+  if (offset < 0) offset = 0
+
+  path.style.strokeDasharray = `${pathLength}`
+  path.style.strokeDashoffset = `${offset}`
+}
+
+scroll()
+window.addEventListener('scroll', scroll)
+ 
